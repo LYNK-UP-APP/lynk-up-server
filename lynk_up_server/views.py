e3ac0bad8b88e88b737090ed3c8c33be0dc37448
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-05-13 18:58:16 -0600

    Add: basic test response to localhost

--- lynk_up_server/views.py
+++ lynk_up_server/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+# Although this is called a view in django, this "API view" actually functions more similarly
+# to a rails controller.
 
-# Create your views here.
+from django.http import JsonResponse
+
+def api_home(request, *args, **kwargs):
+  return JsonResponse({"message": "Hi there, this is your Django API response!!!"})
