dfa40dc9e51d2ad76802a2387504644e61e4b79d
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-05-19 16:34:50 -0600

    Fix: exposed secret key commented in settings

--- lynk_up_server/lynk_up_server/settings.py
+++ lynk_up_server/lynk_up_server/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = "$&wsi81llw70qzbcle(+h%etw=vr5)i4er*-gzm^em(k*zx#c2"
+
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'RENDER' not in os.environ
 DEBUG = False
@@ -37,7 +37,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-# if RENDER_EXTERNAL_HOSTNAME:    
+# if RENDER_EXTERNAL_HOSTNAME:
 #     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
